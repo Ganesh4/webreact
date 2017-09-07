853b2e9828467c88fd6093d7a39f952ecd12fe62
Surender <surender@webcontentor.com>
2017-09-07 19:33:42 +0530

    other component

---
+++ dev/js/components/Apps.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addTodo } from '../actions/index'
+
+import AddTodo from './AddTodo'
+import TodoList from './TodoList'
+
+class Apps extends Component {
+   render() {
+      const { dispatch, visibleTodos } = this.props
+	
+      return (
+         <div>
+            <AddTodo onAddClick = {text => dispatch(addTodo(text))} />
+            <TodoList todos = {visibleTodos}/>
+         </div>
+      )
+   }
+}
+
+function select(state) {
+   return {
+      visibleTodos: state.todos
+   }
+}
+
+export default connect(select)(Apps)
