029f917d690239c0cf67c3324cce0bd411567a89
Surender <surender@webcontentor.com>
2017-09-19 13:57:15 +0530

    selection job data shoiw

--- dev/js/components/jobList.js
+++ dev/js/components/jobList.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getJobById } from '../actions';
+import _ from "lodash";
+import $ from 'jquery';
 
 class App extends React.Component {
  componentDidMount() {
     console.log('I was triggered during componentDidMount', this.props)
+  }
+
+  
+  getValue(id){
+    let str = ''
+    let data = this.props.jobs;
+    let filterValue = _.filter(data,function(v,k){
+                    return v.id === id;
+    })
+    let getFilterValue = filterValue[0];
+    console.log(getFilterValue.directorName)
+    str +='<div>'+ getFilterValue.directorName + '</div>'+
+          '<div>'+getFilterValue.jobreferNumber+'</div>'+
+          '<div>'+getFilterValue.createdDate+'</div>'+
+          '<div>'+getFilterValue.jobName+'</div>'+
+          '<div>'+getFilterValue.managePeopleCount+'</div>';
+
+    $("#summary").html(str);
+    
+  
   }
 
   render(){
@@ -13,8 +35,11 @@ class App extends React.Component {
            {this.props.jobs.map(job =>
                <li key = {job.id} onClick={() => this.props.onJobClick(job)}>
                    {job.id}<span>----------></span>{job.name}
+                   <button onClick={()=> this.getValue(job.id)}>click</button>
+                  
                 </li>
             )}     
+             <span id="summary" style={{height:200,width:200,color:'red'}}></span>
         </ul>
     )
   }
